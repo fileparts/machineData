91a8ab3836663d9deb60eeaf98fe79c8ecebf32a
Dexter Marks-Barber <dexter@file.parts>
2015-07-07 10:16:33 +0100

    C# Classes

---
+++ hardware.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.IO;
+
+namespace socket_client
+{
+    class hardware
+    {
+        //getProcessorCores
+        public static int getProcessorCores()
+        {
+            return Convert.ToInt32(Environment.ProcessorCount);
+        }
+
+        //getProcessorID
+        public static string getProcessorID()
+        {
+            ManagementClass mc = new ManagementClass("win32_processor");
+            ManagementObjectCollection moc = mc.GetInstances();
+
+            String count = string.Empty;
+
+            foreach (ManagementObject mo in moc)
+            {
+                count = mo.Properties["processorID"].Value.ToString();
+                break;
+            }
+            return count;
+        }
+
+        //getProcessorSpeed
+        public static string getProcessorSpeed()
+        {
+            double? GHz = null;
+            using (ManagementClass mc = new ManagementClass("Win32_Processor"))
+            {
+                foreach (ManagementObject mo in mc.GetInstances())
+                {
+                    GHz = 0.001 * (UInt32)mo.Properties["CurrentClockSpeed"].Value;
+                    break;
+                }
+            }
+
+            return GHz.ToString() + " GHz";
+        }
+        //getAmountofRAM
+        public static string getPhysicalMemory()
+        {
+            ManagementScope oMs = new ManagementScope();
+            ObjectQuery oq = new ObjectQuery("SELECT Capacity FROM Win32_PhysicalMemory");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(oMs, oq);
+            ManagementObjectCollection moc = searcher.Get();
+
+            long MemSize = 0;
+            long mCap = 0;
+
+            foreach (ManagementObject mo in moc)
+            {
+                mCap = Convert.ToInt64(mo["Capacity"]);
+                MemSize += mCap;
+            }
+            MemSize = (MemSize / 1024) / 1024;
+            return MemSize.ToString() + " MB";
+        }
+
+        //getRAMSlots
+        public static string getRAMSlots()
+        {
+            int MemSlots = 0;
+
+            ManagementScope oMs = new ManagementScope();
+            ObjectQuery oq = new ObjectQuery("SELECT MemoryDevices FROM Win32_PhysicalMemoryArray");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(oMs, oq);
+            ManagementObjectCollection moc = searcher.Get();
+
+            foreach (ManagementObject mo in moc)
+                MemSlots = Convert.ToInt32(mo["MemoryDevices"]);
+
+            return MemSlots.ToString();
+        }
+
+        //getDiskSpace
+        public static string getDiskSpace()
+        {
+            long Available = 0;
+
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            foreach (DriveInfo d in allDrives)
+                if (d.IsReady == true)
+                    Available += d.AvailableFreeSpace;
+
+            return software.FormatBytes(Available);
+        }
+    }
+}
---
+++ software.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Microsoft.Win32;
+
+namespace socket_client
+{
+    class software
+    {
+        //getmachinename
+        public static string getMachineName()
+        {
+            return System.Environment.MachineName;
+        }
+
+        //getoperatingsystemversion
+        public static string getOperatingSystem()
+        {
+            var os = System.Environment.OSVersion;
+            string currentOS = null;
+
+            switch (os.Platform)
+            {
+                case PlatformID.Win32S:
+                    currentOS = "Windows 3.1";
+                    break;
+                case PlatformID.Win32Windows:
+                    switch (os.Version.Minor)
+                    {
+                        case 0:
+                            currentOS = "Windows 95";
+                            break;
+                        case 10:
+                            currentOS = "Windows 98";
+                            break;
+                        default:
+                            currentOS = "Unknown";
+                            break;
+                    }
+                    break;
+                case PlatformID.Win32NT:
+                    switch (os.Version.Major)
+                    {
+                        case 3:
+                            currentOS = "Windows NT 3.51";
+                            break;
+                        case 4:
+                            currentOS = "Windows NT 4.0";
+                            break;
+                        case 5:
+                            switch (os.Version.Minor)
+                            {
+                                case 0:
+                                    currentOS = "Windows 2000";
+                                    break;
+                                case 1:
+                                    currentOS = "Windows XP";
+                                    break;
+                                case 2:
+                                    currentOS = "Windows 2003";
+                                    break;
+                                default:
+                                    currentOS = "Unknown";
+                                    break;
+                            }
+                            break;
+                        case 6:
+                            switch (os.Version.Minor)
+                            {
+                                case 0:
+                                    currentOS = "Windows Vista";
+                                    break;
+                                case 1:
+                                    currentOS = "Windows 7";
+                                    break;
+                                case 2:
+                                    currentOS = "Windows 8";
+                                    break;
+                                case 3:
+                                    currentOS = "Windows 8.1";
+                                    break;
+                                default:
+                                    currentOS = "Unknown";
+                                    break;
+                            }
+                            break;
+                        case 10:
+                            currentOS = "Windows 10";
+                            break;
+                        default:
+                            currentOS = "Unknown";
+                            break;
+                    }
+                    break;
+                case PlatformID.WinCE:
+                    currentOS = "Windows CE";
+                    break;
+                case PlatformID.Unix:
+                    currentOS = "Unix";
+                    break;
+                default:
+                    currentOS = "Unknown";
+                    break;
+            };
+            return currentOS;
+        }
+
+        //getoperatingsystembit
+        public static string getOperatingSystemBit()
+        {
+            string check = System.Environment.Is64BitOperatingSystem.ToString();
+            string bit = string.Empty;
+
+            if (check == "True")
+            {
+                bit = "64-bit";
+            }
+            else
+            {
+                bit = "32-bit";
+            }
+            return bit;
+        }
+
+        //getmacaddress
+        public static string getMACAddress()
+        {
+            string host = Dns.GetHostName();
+            string ip = Dns.GetHostEntry(host).AddressList[0].ToString();
+
+            return ip;
+        }
+
+        //getipaddress
+        public static string getIPAddress()
+        {
+            string host = Dns.GetHostName();
+            int length = Dns.GetHostEntry(host).AddressList.Length;
+            string ip = string.Empty;
+
+            if (length == 2)
+                ip = Dns.GetHostEntry(host).AddressList[1].ToString();
+            else
+                ip = Dns.GetHostEntry(host).AddressList[2].ToString();
+
+            return ip;
+        }
+
+        //getcurrentuser
+        public static string getCurrentUser()
+        {
+            string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            return userName;
+        }
+
+        //check if application is installed
+        public static bool check(string app)
+        {
+            string displayName;
+            RegistryKey key;
+
+            //check current user
+            key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall");
+            if (key != null)
+            {
+                foreach (string keyName in key.GetSubKeyNames())
+                {
+                    RegistryKey subkey = key.OpenSubKey(keyName);
+                    displayName = subkey.GetValue("DisplayName") as string;
+                    if (displayName != null && displayName.ToUpper().Contains(app.ToUpper()))
+                        return true;
+                }
+                key.Close();
+            }
+
+            //check windows 32
+            key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall");
+            if (key != null)
+            {
+                foreach (string keyName in key.GetSubKeyNames())
+                {
+                    RegistryKey subkey = key.OpenSubKey(keyName);
+                    displayName = subkey.GetValue("DisplayName") as string;
+                    if (displayName != null && displayName.ToUpper().Contains(app.ToUpper()))
+                        return true;
+                }
+                key.Close();
+            }
+
+            //check windows 64
+            key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall");
+            if (key != null)
+            {
+                foreach (string keyName in key.GetSubKeyNames())
+                {
+                    RegistryKey subkey = key.OpenSubKey(keyName);
+                    displayName = subkey.GetValue("DisplayName") as string;
+                    if (displayName != null && displayName.ToUpper().Contains(app.ToUpper()))
+                        return true;
+                }
+                key.Close();
+            }
+            return false;
+        }
+
+        //convert bytes
+        public static string FormatBytes(long bytes)
+        {
+            string[] Suffix = { "B", "KB", "MB", "GB", "TB" };
+            int i;
+            double dblSByte = bytes;
+
+            for (i = 0; i < Suffix.Length && bytes >= 1024; i++, bytes /= 1024)
+                dblSByte = bytes / 1024.0;
+
+            return String.Format("{0:0.##} {1}", dblSByte, Suffix[i]);
+        }
+    }
+}
